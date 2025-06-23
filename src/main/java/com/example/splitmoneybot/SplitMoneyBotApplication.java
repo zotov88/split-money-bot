9f7aa9bc3151d1f0877cede2d9515ac6ac4a5e57
zotov88 <zotov_l88@mail.ru>
2025-06-23 23:27:39 +0300

    init commit

---
+++ src/main/java/com/example/splitmoneybot/SplitMoneyBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.splitmoneybot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitMoneyBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplitMoneyBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/splitmoneybot/SplitMoneyBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.splitmoneybot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitMoneyBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
